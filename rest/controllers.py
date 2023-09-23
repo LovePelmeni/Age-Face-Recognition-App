f2c6b4aee26206090cd301158e21d47aa27de679
= <kirklimushin@gmail.com>
2023-09-23 22:05:43 +0300

    Updated Preprocessing Notebooks, removed extra code lines

--- rest/controllers.py
+++ rest/controllers.py
@@ -13,19 +13,6 @@ Logger.addHandler(file_handler)
 # Loading Neural Network..
 try:
     model = torch.jit.load("experiments/current_experiment/prod_models/neural_net.onnx")
-
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-        Logger.debug("Model has been attached to available GPU, which supports CUDA")
-
-    elif backends.mps.is_available():
-        device = torch.device("mps")
-        Logger.debug("Model has been attached to available MPS backend.")
-    else:
-        device = torch.device("cpu")
-        Logger.debug("cuda and mps are not available, attach model to CPU")
-
-    model.to(device)
     # entering evaluation mode for Neural Network
     model.eval()
     
